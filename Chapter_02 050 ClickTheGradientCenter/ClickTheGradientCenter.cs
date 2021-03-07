ab10c872bc72d54873de846c0093f825786d6f2b
nvbtds@gmail.com <nvbtds@gmail.com>
2021-03-08 02:49:35 +0500

    Reference repaired and get connected .cs and project

--- Chapter_02 050 ClickTheGradientCenter/ClickTheGradientCenter.cs
+++ Chapter_02 050 ClickTheGradientCenter/ClickTheGradientCenter.cs
@@ -20,12 +20,12 @@ namespace WpfApplication1
         {
             Title = "Click the Gradient Center";  // Задаем заголовок окна
             brush = new RadialGradientBrush(Colors.White, Colors.DarkGreen);
-            
+
             brush.RadiusX = brush.RadiusY = 0.1;  // Задаем полуоси эллипса радиального градиента
-            
+
             // Настройка, из-за которой градиент повторяется в исходном порядке, пока не будет заполнено все пространство
             brush.SpreadMethod = GradientSpreadMethod.Repeat;
-            
+
             Background = brush;  // задаем кисть, описывающую фон окна (в нашем случае это радиальный градиент)
         }
 
@@ -35,7 +35,7 @@ namespace WpfApplication1
             // (та область, которая в нашем случае закрашивается градиентом)
             var width = ActualWidth  // отображаемая ширина окна (т.е. с учетом вертикальных границ рамки по бокам)
                         - 2 * SystemParameters.ResizeFrameVerticalBorderWidth;  // вычитаем толщину 2-х вертикальных границ (левой и правой)
-            
+
             // height - это высота внутренней области окна
             var height = ActualHeight  // отображаемая высота окна (т.е. с учетом горизонтальных границ рамки)
                          - 2 * SystemParameters.ResizeFrameHorizontalBorderHeight  // вычитаем толщину 2-х горизонтальных границ (верхней и нижней)
@@ -43,7 +43,7 @@ namespace WpfApplication1
 
             // Получаем положение курсора в окне, в абсолютных величинах
             var ptMouse = args.GetPosition(this);
-            
+
             // Переводим в относительные величины, чтобы в дальнейшем изменять параметры градиента
             ptMouse.X /= width;
             ptMouse.Y /= height;
