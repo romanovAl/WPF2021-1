fe509a201dced24704013066b03ca9c6b6a58112
nvbtds@gmail.com <nvbtds@gmail.com>
2021-03-24 22:11:13 +0500

    1

---
+++ Chapter_01 024 InheritAppAndWindow/MyApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.InheritAppAndWindow
+{
+    class MyApplication : Application
+    {
+        protected override void OnStartup(StartupEventArgs args) //Запускает код при запуске приложения
+        {
+            base.OnStartup(args); 
+            MyWindow win = new MyWindow();
+            win.Show(); //Открывает окно
+        }
+    }
+}
---
+++ Chapter_01 024 InheritAppAndWindow/MyWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+namespace Petzold.InheritAppAndWindow
+{
+    public class MyWindow : Window 
+    { 
+        //Конструктор класса MyWindow
+        public MyWindow() 
+        { 
+            Title = "Inherit App & Window";
+        }
+        protected override void OnMouseDown(MouseButtonEventArgs args) //запускаем блок кода при нажатии мышки
+        { 
+            base.OnMouseDown(args);
+            string strMessage = string.Format("Window clicked with  {0} button at point ({1})", args.ChangedButton,/*Какая кнопка*/ args.GetPosition(this) /*Позиция нажатия*/); 
+            MessageBox.Show(strMessage, Title); //Отображает окно сообщения
+        } 
+    }
+}
