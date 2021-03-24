3b8bfda0d9240a2bde693e1c200a5fb1b2e00fa5
romanovAl <ml.naslednik2001@gmail.com>
2021-03-24 20:47:23 +0500

    Добавил комментарии

---
+++ Chapter_03 071 RenderTheGraphic/SimpleEllipse.cs
@@ -0,0 +1,22 @@
+﻿//---------------------------------------------- 
+//SimpleEllipse.cs (c) 2006 by Charles Petzold 
+//----------------------------------------------
+
+
+using System; 
+using System.Windows; 
+using System.Windows.Media; 
+
+namespace Petzold.RenderTheGraphic 
+{     
+    class SimpleEllipse : FrameworkElement     
+    {         
+        protected override void OnRender (DrawingContext dc)         //DrawingContext используется для рисования вызовом DrawEllipse
+        {             
+            dc.DrawEllipse(Brushes.Blue, new Pen (Brushes.Red, 24),     //Задаем цвет внутренности и границы (в случае границы создаем кастомную кисть со своим цветом и шириной)     
+                new Point(RenderSize.Width / 4,  RenderSize.Height / 4),                 
+                RenderSize.Width / 4, RenderSize .Height / 4);         
+        }     
+    } 
+} 
+
