f0aa1d1140374e9ca20bb58f11487b0e73fbe528
Andrey Rechkalov <61356488+BizonAR@users.noreply.github.com>
2021-03-24 17:17:59 +0500

    Andrey Rechkalov

---
+++ Chapter_03 066 FormatTheText/FormatTheText.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Documents;
+
+namespace FormatTheText
+{
+    class FormatTheText : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new FormatTheText());
+        }
+
+        public FormatTheText() //Функция форматирования текста
+        {
+            Title = "Format The Text"; //Заголовок
+            TextBlock txt = new TextBlock(); //Объявляется текст - объект класса TextBlock
+            txt.FontSize = 32; //Задаётся размер шрифта текста
+            txt.Inlines.Add("This is some "); //Строка "This is some " добавляется в текст
+            txt.Inlines.Add(new Italic(new Run("italic"))); //Строка "italic", выделенная курсивом добавляется в текст
+            txt.Inlines.Add(" text, and this is some "); //Строка " text, and this is some " добавляется в текст
+            txt.Inlines.Add(new Bold(new Run("bold"))); //Строка "bold", выделенная жирным добавляется в текст
+            txt.Inlines.Add(" text, and let's cap it off with some "); //Строка " text, and let's cap it off with some " добавляется в текст
+            txt.Inlines.Add(new Bold(new Italic(new Run("bold italic")))); //Строка "bold italic", выделенная жирным курсивом добавляется в текст
+            txt.Inlines.Add(" text."); //Строка " text." добавляется в текст
+            txt.TextWrapping = TextWrapping.Wrap; //Перенос текста на новую строку в случае недостаточного размера окна
+
+            Content = txt; //Свойству класса ContentControl задаётся значение получившегося текста
+        }
+    }
+}
---
+++ Chapter_04 090 UriDialog/UriDialog.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+namespace nikbarale.NavigateTheWeb
+{
+    class UriDialog : Window//Uri dialog наследик Window
+    {
+        TextBox txtbox;//создаем класс txtboc с помощью которого пользователь может вводить текст в приложении
+        public UriDialog()//задаем значения нескольких свойств класса
+        {
+            Title = "Enter a URI";//заголовок окна
+            ShowInTaskbar = false;//не показывается в панеле задач
+            SizeToContent = SizeToContent.WidthAndHeight;//автоматически изменяет размер по размеру содержимого
+            WindowStyle = WindowStyle.ToolWindow;//задаем стиль границы окна( допускается изменение размеров окна, но не имеет кнопок сворачивания/разворачивания)
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;// открывется по центру своего владельца
+            txtbox = new TextBox();//инициализируем класс txtbox
+            txtbox.Margin = new Thickness(48);//Свойство Margin определяет поле вокруг элемента управления, Thickness определяет толщину рамки
+            Content = txtbox;//задаем содержимое обьекта ContentControl в виде нашего txtbox
+            txtbox.Focus();//Возвращает значение, указывающее, имеется ли на txtbox фокус ввода.
+        }
+        public string Text//предоставляем доступ к свойству text элемента txtbox
+        {
+            set
+            {
+                txtbox.Text = value;//получаем строку
+                txtbox.SelectionStart = txtbox.Text.Length;//перемещаем точку вставки справа от последнего символа
+            }
+            get
+            {
+                return txtbox.Text;//возвращаем полученную строку
+            }
+        }
+        protected override void OnKeyDown(KeyEventArgs args)//переопределяем метод OnKeyDown, что бы можно было закрыть окно клавишей enter, KeyEventArgs - класс включающий в себя Key 
+        {
+            if (args.Key == Key.Enter) Close();// args.key получет данные с клавиатуры  
+        }
+    }
+    public class NavigateTheWeb : Window
+    {
+        Frame frm; // объявление класса Frame
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new NavigateTheWeb());
+        }
+        public NavigateTheWeb()
+        {
+            Title = "Navigate the Web"; // Название главного окна
+            frm = new Frame(); // создание объекта класса Frame
+            Content = frm; // установка контента на главное окно
+            Loaded += OnWindowLoaded; // Покажет результат, только когда dlg в 57-ой строке будет создан и готов к использованию
+        }
+        void OnWindowLoaded(object sender, RoutedEventArgs args)
+        {
+            UriDialog dlg = new UriDialog(); // создание диалогового окна window с использованием класса UriDialog
+            dlg.Owner = this; // Владельцем становится текущий объект
+            dlg.Text = "http://"; // Задаёт значение атрибута Text txtbox
+            dlg.ShowDialog(); // Показывает это окно внутри главного окна
+            try
+            {
+                frm.Source = new Uri(dlg.Text); // frm пытается получить ввод от пользователя и открыть веб страницу по этой ссылке
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, Title); // В случае любого исключения показать диалоговое окно пользователю с ошибкой
+            }
+        }
+    }
+}
