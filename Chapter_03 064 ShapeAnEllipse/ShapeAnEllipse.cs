2c96c833bdcf0c31a094a46d6a9d63fb4aa2a140
MrRooots <46628524+MrRooots@users.noreply.github.com>
2021-03-26 13:18:31 +0500

    Chapter 3-4. 0_o

---
+++ Chapter_03 064 ShapeAnEllipse/ShapeAnEllipse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Chapter_03_xx_ShapeAnEllipse {
+  internal class ShapeAnEllipse : Window {
+    [STAThread]
+    public static void Main() {
+      new Application().Run(new ShapeAnEllipse());  // Standard program run
+    }
+    
+    // My sub function that create a new LinearGradientBrush object and return it
+    private static LinearGradientBrush CreateGradient() {
+      // LinearGradient params: startColor, endColor, startPoint, endPoint
+      return new LinearGradientBrush(Colors.CadetBlue, Colors.Chocolate, new Point(1, 0), new Point(0, 1));
+    }
+    
+    // Ellipse class itself
+    private ShapeAnEllipse() {
+      Title = "Shape an Ellipse";
+      // Fill - color inside ellipse, StrokeThickness - width of ellipse outline, Stroke - fill color of outline
+      Content = new Ellipse {Fill = Brushes.AliceBlue, StrokeThickness = 24, Stroke = CreateGradient()};;
+    }
+  }
+}
---
+++ Chapter_04 080 CommandTheButton/CommandTheButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+// Program will paste the clipboard content into main window title
+namespace Chapter_04_xxx_CommandTheButton {
+  public class CommandTheButton : Window {
+    [STAThread]
+    public static void Main() {
+      new Application().Run(new CommandTheButton());
+    }
+
+    private CommandTheButton() {
+      Title = "Command the Button";  // Create the Button and set as window content.
+      
+      // Bind the command to the event handlers.
+      Content = new Button {
+        // Centering the button in window
+        HorizontalAlignment = HorizontalAlignment.Center,
+        VerticalAlignment = VerticalAlignment.Center,
+        // Application commands - class that contains special commands like copy/paste etc.
+        Command = ApplicationCommands.Paste,
+        Content = ApplicationCommands.Paste.Text
+      }; 
+      // Create new instance of CommandBinding class with command (property), executor and execution ability checker (functions)
+      // Then bind this command to button binds
+      CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, PasteOnExecute, PasteCanExecute));
+    }
+    
+    // If clipboard not empty will change the window title with clipboard content
+    private void PasteOnExecute(object sender, ExecutedRoutedEventArgs args) {
+      Title = Clipboard.GetText();
+    }
+    
+    // Check if clipboard not empty
+    private static void PasteCanExecute(object sender, CanExecuteRoutedEventArgs args) {
+      // Asks clipboard if it contains text in unicode (true) else (false)
+      args.CanExecute = Clipboard.ContainsText();
+    }
+
+    protected override void OnMouseDown(MouseButtonEventArgs args) {
+      base.OnMouseDown(args);
+      Title = "Command the Button";
+    }
+  }
+}
